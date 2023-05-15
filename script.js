4fb2c1da431725ad15b7b36b9f2d442056a28231
AdrielBori <103082801+AdrielBori@users.noreply.github.com>
2023-05-15 19:27:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,349 @@
+
+const items=[
+            {
+            "nombre":"valiant golves",
+            "luck":1,
+            "dañoEx":0,
+            "daño2":0,
+            "velocidad":0,
+            "level20":0,
+            "nivel":0,
+            "raza":"DM",
+            "ReduceDamage":1,
+            "nivel":0,
+            "Rate":0,
+            "Reflect":1,
+            "HPmax":0,
+            "tipo":"arma"
+            },{
+                "nombre":"platina staff",
+                "luck":1,
+                "dañoEx":0,
+                "daño2":1,
+                "velocidad":0,
+                "level20":1,
+                "nivel":0,
+                "raza":"GM",
+                "ReduceDamage":0,
+                "nivel":0,
+                "Rate":0,
+                "Reflect":0,
+                "HPmax":0,
+                "tipo":"arma"
+                },{
+                    "nombre":"venom golves",
+                    "luck":1,
+                    "dañoEx":0,
+                    "daño2":0,
+                    "velocidad":0,
+                    "level20":0,
+                    "nivel":0,
+                    "raza":"GM",
+                    "ReduceDamage":1,
+                    "nivel":0,
+                    "Rate":0,
+                    "Reflect":1,
+                    "HPmax":0,
+                    "tipo":"arma"
+                    },{
+                        "nombre":"Sylphid armor",
+                        "luck":1,
+                        "dañoEx":0,
+                        "daño2":0,
+                        "velocidad":0,
+                        "level20":0,
+                        "nivel":0,
+                        "raza":"HE",
+                        "ReduceDamage":0,
+                        "nivel":0,
+                        "Rate":0,
+                        "Reflect":1,
+                        "HPmax":1,
+                        "tipo":"arma"
+                        },{
+                            "nombre":"Grand Soul golves",
+                            "luck":1,
+                            "dañoEx":0,
+                            "daño2":0,
+                            "velocidad":0,
+                            "level20":0,
+                            "nivel":0,
+                            "raza":"GM",
+                            "ReduceDamage":0,
+                            "nivel":0,
+                            "Rate":0,
+                            "Reflect":1,
+                            "HPmax":1,
+                            "tipo":"arma"
+                            },{
+                                "nombre":"Eclipse boot",
+                                "luck":1,
+                                "dañoEx":0,
+                                "daño2":0,
+                                "velocidad":0,
+                                "level20":0,
+                                "nivel":0,
+                                "raza":"GM",
+                                "ReduceDamage":1,
+                                "nivel":0,
+                                "Rate":0,
+                                "Reflect":0,
+                                "HPmax":1,
+                                "tipo":"arma"
+                                },{
+                                    "nombre":"Sylphid boot",
+                                    "luck":1,
+                                    "dañoEx":0,
+                                    "daño2":0,
+                                    "velocidad":0,
+                                    "level20":0,
+                                    "nivel":0,
+                                    "raza":"HE",
+                                    "ReduceDamage":0,
+                                    "nivel":0,
+                                    "Rate":0,
+                                    "Reflect":1,
+                                    "HPmax":1,
+                                    "tipo":"arma"
+                                    },{
+                                        "nombre":"iris pant",
+                                        "luck":1,
+                                        "dañoEx":0,
+                                        "daño2":0,
+                                        "velocidad":0,
+                                        "level20":0,
+                                        "nivel":0,
+                                        "raza":"HE",
+                                        "ReduceDamage":1,
+                                        "nivel":0,
+                                        "Rate":1,
+                                        "Reflect":0,
+                                        "HPmax":0,
+                                        "tipo":"arma"
+                                        },{
+                                            "nombre":"Sunlight",
+                                            "luck":1,
+                                            "dañoEx":0,
+                                            "daño2":0,
+                                            "velocidad":0,
+                                            "level20":0,
+                                            "nivel":0,
+                                            "raza":"LE",
+                                            "ReduceDamage":1,
+                                            "nivel":0,
+                                            "Rate":0,
+                                            "Reflect":0,
+                                            "HPmax":1,
+                                            "tipo":"arma"
+                                            },
+    {
+            "nombre":"Great Dragon golves",
+            "luck":0,
+            "dañoEx":0,
+            "daño2":0,
+            "velocidad":0,
+            "level20":0,
+            "nivel":0,
+            "raza":"BM",
+            "ReduceDamage":1,
+            "nivel":0,
+            "Rate":0,
+            "Reflect":1,
+            "HPmax":0,
+            "tipo":"arma"
+            },{
+                "nombre":"Eclipse Helm",
+                "luck":1,
+                "dañoEx":0,
+                "daño2":0,
+                "velocidad":0,
+                "level20":0,
+                "nivel":0,
+                "raza":"GM",
+                "ReduceDamage":1,
+                "nivel":0,
+                "Rate":1,
+                "Reflect":0,
+                "HPmax":0,
+                "tipo":"arma"
+                },{
+                    "nombre":"Eclipse Golves",
+                    "luck":1,
+                    "dañoEx":0,
+                    "daño2":0,
+                    "velocidad":0,
+                    "level20":0,
+                    "nivel":0,
+                    "raza":"GM",
+                    "ReduceDamage":1,
+                    "nivel":0,
+                    "Rate":1,
+                    "Reflect":0,
+                    "HPmax":0,
+                    "tipo":"arma"
+                    },{
+                        "nombre":"Eclipse Pant",
+                        "luck":1,
+                        "dañoEx":0,
+                        "daño2":0,
+                        "velocidad":0,
+                        "level20":0,
+                        "nivel":0,
+                        "raza":"GM",
+                        "ReduceDamage":1,
+                        "nivel":0,
+                        "Rate":1,
+                        "Reflect":0,
+                        "HPmax":0,
+                        "tipo":"arma"
+                        },{
+                            "nombre":"Ashcrow golves",
+                            "luck":0,
+                            "dañoEx":0,
+                            "daño2":0,
+                            "velocidad":0,
+                            "level20":0,
+                            "nivel":0,
+                            "raza":"BM",
+                            "ReduceDamage":1,
+                            "nivel":0,
+                            "Rate":0,
+                            "Reflect":1,
+                            "HPmax":0,
+                            "tipo":"arma"
+                            },{
+                                "nombre":"Explosion Blade",
+                                "luck":1,
+                                "dañoEx":1,
+                                "daño2":0,
+                                "velocidad":0,
+                                "level20":1,
+                                "nivel":0,
+                                "raza":"DM",
+                                "ReduceDamage":0,
+                                "nivel":0,
+                                "Rate":0,
+                                "Reflect":0,
+                                "HPmax":0,
+                                "tipo":"arma"
+                                },{
+                                    "nombre":"Platina staff",
+                                    "luck":1,
+                                    "dañoEx":1,
+                                    "daño2":0,
+                                    "velocidad":1,
+                                    "level20":0,
+                                    "nivel":0,
+                                    "raza":"DM",
+                                    "ReduceDamage":0,
+                                    "nivel":0,
+                                    "Rate":0,
+                                    "Reflect":0,
+                                    "HPmax":0,
+                                    "tipo":"arma"
+                                    },{
+                                        "nombre":"Viper",
+                                        "luck":1,
+                                        "dañoEx":1,
+                                        "daño2":0,
+                                        "velocidad":0,
+                                        "level20":1,
+                                        "nivel":0,
+                                        "raza":"DM",
+                                        "ReduceDamage":0,
+                                        "nivel":13,
+                                        "Rate":0,
+                                        "Reflect":0,
+                                        "HPmax":0,
+                                        "tipo":"arma"
+                                        }
+]
+
+
+
+const ArrayFilter=[]
+
+let cambio=(x)=>{
+    localStorage.setItem("filtro",x)
+    window.location.reload()
+}
+
+
+
+    const restaurar=localStorage.getItem("filtro")
+                
+                    items.forEach(element => {
+                        if(element.raza==restaurar){
+                            ArrayFilter.push(element)
+                        }
+                    });
+                    
+                    console.log(ArrayFilter)
+        const tienda=document.getElementById("principal")
+        ArrayFilter.forEach(element => {
+            const conteiner= document.createElement('div')
+            conteiner.classList.add("conteiner_items")
+            const tarjeta=document.createElement('div')
+            tarjeta.classList.add("card")
+            tarjeta.innerHTML=`
+                    <span class="nombP"> Exelent ${element.nombre} + ${element.nivel}</span>
+                    <span class="opcion1">${element.luck>0 ? "luck(critical damage +5%)":""}</span>
+                    <span >daño adicional+</span>
+                    <span class="opcion2">${element.dañoEx>0 ? "Daño Excelent":""}</span>
+                    <span class="opcion3">${element.daño2>0 ? "Daño 2%":""}</span>
+                    <span class="opcion3">${element.velocidad>0 ? "velocidad":""}</span>
+                    <span class="opcion3">${element.level20>0 ? "daño +level20":""}</span>
+                    <span class="opcion2">${element.ReduceDamage>0 ? "reduce damage":""}</span>
+                    <span class="opcion3">${element.Rate>0 ? "Rate %":""}</span>
+                    <span class="opcion3">${element.Reflect>0 ? "reflect":""}</span>
+                    <span class="opcion3">${element.HPmax>0 ? "HP MAX":""}</span>
+                    <a href="https://wa.me/541133830684?text=quiero${element.nombre}"><img src="whatsapp.png" alt="" class="iconw"></a>
+                    
+                `
+            conteiner.appendChild(tarjeta);
+            tienda.appendChild(conteiner);
+        })
+        
+
+
+
+
+/*
+items[0].armas.forEach(element => {
+    const conteiner= document.getElementById('conteiner_items')
+    const tarjeta=document.createElement('div')
+    tarjeta.classList.add("card")
+    tarjeta.innerHTML=`
+        
+            <span class="nombP"> Exelent ${element.nombre} + ${element.nivel}</span>
+            <span class="opcion1">${element.luck>0 ? "luck(critical damage +5%)":""}</span>
+            <span >daño adicional+</span>
+            <span class="opcion2">${element.dañoEx>0 ? "Daño Excelent":""}</span>
+            <span class="opcion3">${element.daño2>0 ? "Daño 2%":""}</span>
+            <span class="opcion3">${element.velocidad>0 ? "velocidad":""}</span>
+            <span class="opcion3">${element.level20>0 ? "daño +level20":""}</span>
+        `
+    conteiner.appendChild(tarjeta);
+})
+
+items[1].armadura.forEach(element => {
+    const conteiner= document.getElementById('conteiner_items')
+    const tarjeta=document.createElement('div');
+    tarjeta.classList.add("card")
+    tarjeta.innerHTML=`
+        
+            <span class="nombP">Exelent ${element.nombre} + ${element.nivel}</span>
+            <span class="opcion1">${element.luck>0 ? "luck(critical damage +5%)":""}</span>
+            <span >daño adicional+</span>
+            <span class="opcion2">${element.ReduceDamage>0 ? "reduce damage":""}</span>
+            <span class="opcion3">${element.Rate>0 ? "Rate %":""}</span>
+            <span class="opcion3">${element.Reflect>0 ? "reflect":""}</span>
+            <span class="opcion3">${element.HPmax>0 ? "HP MAX":""}</span>
+        `
+    conteiner.appendChild(tarjeta);
+})
+
+*/
+
+
+
